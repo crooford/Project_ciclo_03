08064b1b696ae495ffcd54c47d4f1ca402bcf533
Gabriel1050 <gabrielolayah@gmail.com>
2022-08-29 14:29:10 -0500

    Se configuro mysql

--- libreria/urls.py
+++ libreria/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.inicio, name='inicio'),
+    path('', views.login, name='login'),
     path('login', views.login, name='login'),
     path('admon.html', views.admon, name='admon'),
     path('admon-waiter.html', views.waiter, name='waiter'),
