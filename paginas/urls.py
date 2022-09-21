93ec28e9b2f7e06ba9d8208b565d20ddef72c170
Leonnarddo <leonnarddo@gmail.com>
2022-09-21 11:59:35 -0500

    menu

--- paginas/urls.py
+++ paginas/urls.py
@@ -6,8 +6,9 @@ urlpatterns = [
    path('', views.ppal, name='ppal'),
    path('ppal.html', views.ppal, name='ppal'),
    path('registro.html', views.registro, name='registro'),  
-   path('usuarios.html', views.usuarios, name='usuarios'), 
-   path('mesero-mesas.html', views.mesero_mesas, name='mesero-mesas'), 
-   path('mesero-orden.html', views.mesero_orden, name='mesero-orden'), 
-   
+   path('usuarios.html', views.usuarios, name='usuarios'),
+   path('menu.html', views.menu, name='menu'),
+   path('crear-plato.html', views.crear_plato, name='crear-plato'),
+   path('editar-contacto/<int:id>/', views.editar_plato, name='editar-plato'), 
+   path('eliminar-contacto/<int:id>/', views.eliminar_plato, name='eliminar-plato'),
 ]
--- paginas/views.py
+++ paginas/views.py
@@ -1,15 +1,15 @@
 from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import CustomUserCreationForm, Usuario
 from django.contrib.auth import login, logout, authenticate
+from .forms import Menuform
+from .models import Menu
 
 # Create your views here.
 def usuarios(request):
     return render(request, 'usuarios.html')
-
 def ppal(request):
     return render(request, 'ppal.html')
-
 def registro(request):
    
     if request.method == 'POST':
@@ -29,14 +29,44 @@ def registro(request):
             return render(request, 'ppal.html')
     else:
         formulario = CustomUserCreationForm()
-        usuario_form = Usuario()
-                
-        
+        usuario_form = Usuario()     
     context = {'formulario' : formulario, 'usuario_form': usuario_form}    
     return render(request, 'registration/registro.html', context)
 
-def mesero_mesas(request):
-    return render(request, 'mesero-mesas.html')
+def menu(request):
+    platos = Menu.objects.all()
+    context = {'platos': platos}
+    return render(request,'menu.html', context)
 
-def mesero_orden(request):
-    return render(request, 'mesero-orden.html')
+def crear_plato(request):
+    if request.method == 'GET':
+        formulario = Menuform()
+    else:
+         formulario = Menuform(request.POST)
+         if formulario.is_valid():
+             formulario.save()
+             return redirect('menu.html')    
+    return render(request, 'crear-plato.html',{'formulario': formulario})
+
+def editar_plato(request,id):
+    plato= Menu.objects.get(id=id)
+    if request.method == 'POST':
+        formulario = Menuform(instance=plato)
+        contexto={
+            'formulario': formulario
+        }
+    else:
+        formulario = Menuform(request.POST,instance=plato)
+        contexto={
+            'formulario': formulario
+        }
+        if formulario.is_valid():
+            formulario.save()
+            return redirect('menu.html')
+    return render(request,'editar_plato.html',contexto) 
+
+def eliminar_plato(request,id):
+    plato = Menu.objects.get(id=id)
+    plato.delete()
+    return redirect('menu.html')
+
