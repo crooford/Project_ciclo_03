4a5c9e03aefeb3b1257fb92ecb522d6092959283
Carlos Escudero <escuderocarlos23@gmail.com>
2022-10-03 14:55:58 -0500

    new db

--- Tworden/settings.py
+++ Tworden/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'Tworden.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':'twordendb',
+        'USER': 'twordenmanage',
+        'PASSWORD': 'G0LscU0o2mgXWsqp3Y0RtNSostyQfUI1',
+        'HOST': 'dpg-cctjniun6mptlbt1ohj0-a',
+        'PORT': 5432
     }
 }
 
